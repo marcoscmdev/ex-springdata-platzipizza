9432b57a3358f989c92fff3a533b686c44a0ff44
MarcosCM <castromirandamarcos@gmail.com>
2026-07-29 14:52:17 +0200

    feat: add order entity and fetch relation

--- src/main/java/com/platzi/pizza/persistence/entity/OrderEntity.java
+++ src/main/java/com/platzi/pizza/persistence/entity/OrderEntity.java
@@ -1,5 +1,6 @@
 package com.platzi.pizza.persistence.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,10 +30,11 @@ public class OrderEntity {
     @Column(name = "additional_notes", length = 200)
     private String addionalNotes;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_customer", referencedColumnName = "id_customer",  insertable = false, updatable = false)
+    @JsonIgnore
     private CustomerEntity customer;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", fetch = FetchType.EAGER)
     private List<OrderItemEntity>items;
 }
